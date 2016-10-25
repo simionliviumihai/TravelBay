4591201520bd5ec443de6ce620b311d53c5b3892
Liviu Simion <simionliviumihai@outlook.com>
2016-10-24 23:08:14 -0700

    Adding test libs and favicon(broken)

--- app.js
+++ app.js
@@ -15,14 +15,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+// enable testing
+app.use(function(req, res, next){
+ res.locals.showTests = app.get('env') !== 'production' &&
+ req.query.test === '1';
+ next();
+});
+
+// routes
 app.use('/', routes);
 app.use('/about', about);
 app.use('/users', users);
